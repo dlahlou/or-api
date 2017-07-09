9f9b91152a90f73682ac9e907bdefa46befc531a
dlahlou <dlahlou@google.com>
2017-07-09 19:07:47 +0100

    add knapsack model

--- api/services/knapsackService.js
+++ api/services/knapsackService.js
@@ -1,22 +1,23 @@
 
 module.exports = {
   solve: function(){
-    var solver = require('minizinc-solver');
-/*    var problem = 'int: n=3; % number of objects'
-    + 'set of int: OBJ = 1..n; '
-    + 'int: capacity=9; '
-    + 'array[OBJ] of int: profit = [15,10,7]; '
-    + 'array[OBJ] of int: size = [4,3,2]; '
-    + 'array[OBJ] of var int: x; % how many of each object '
-    + 'constraint forall(i in OBJ)(x[i] >= 0); '
-    + 'constraint sum(i in OBJ)(size[i] * x[i]) <= capacity; '
-    + 'solve maximize sum(i in OBJ)(profit[i] * x[i]); '
-    + 'output ["x = ", show(x), ];'
-    + 'solver.solve(problem).then(doSomethingWithProblemOutput); ';*/
+   
+    const { spawn } = require('child_process');
+    const ls = spawn('minizinc', ['knapsack.mzn', 'knapsack0.dzn']);
 
-    var problem = 'solve satisfy; output ["hi"];';
+    //const ls = spawn('pwd', []);
 
-    console.log(solver.solve(problem));
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
   }
 }
 
